e205b3cab6f4f86a28466d2f92f9f28232089398
anvelezec <anvelezec@unal.edu.co>
2018-10-28 21:49:57 -0500

    bagging

--- mayorityVoteSimple.py
+++ mayorityVoteSimple.py
@@ -10,6 +10,6 @@ import numpy as np
 
 # Equal weight distribution 
 np.argmax(np.bincount([0, 0, 1],weights=[1, 1, 1]))
-   
+
 # Assigns classification accoundingly to weight distribution
 np.argmax(np.bincount([0, 0, 1],weights=[0.2, 0.2, 0.6]))
